7dba5f96900814e6186c2e4c0707f902766e236a
Steve Almond <steve@SilkAndSlug.com>
2016-09-09 20:10:16 +0100

    Add bootstrapping.
    Signed-off-by: Steve Almond <steve@SilkAndSlug.com>

---
+++ habitica.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+########
+## Silk's Habitica Scripts
+########
+#
+# Wrappers for Habitica's v3 API
+#
+#########
+
+
+## bootstrap
+set -u;	# treat undefined vars as errors
+set -e;	# halt on error
