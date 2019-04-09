737736d80de5f066ecc10d8c19125d4bb6f4e189
Steve Almond <steve@SilkAndSlug.com>
2019-04-09 21:52:19 +0100

    Delete MYSQL_*
    Signed-off-by: Steve Almond <steve@SilkAndSlug.com>

--- includes/config-dev.php
+++ includes/config-dev.php
@@ -1,10 +1,3 @@
 <?php
 
-define('MYSQL_SERVER', 'localhost');
-define('MYSQL_DATABASE', '??');
-define('MYSQL_USERNAME', '??');
-define('MYSQL_PASSWORD', '??');
-define('MYSQL_SUPERUSER_USERNAME', MYSQL_USERNAME);
-define('MYSQL_SUPERUSER_PASSWORD', MYSQL_PASSWORD);
-
 define('SILKANDSLUG_IP', '10.246.125.111');
